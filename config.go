47f67954fcfe33663d8ab8d57d323fee1aec4724
Philopater Shenouda <Philopater.Pentester@outlook.com>
2025-10-08 01:18:02 +0300

    feat: Add .gitignore to exclude config file

--- config.go
+++ config.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"os"
-)
-
+// WARNING: Storing API keys directly in code is a security risk.
 var apiKeys = map[string]string{
-	"urlscan":     os.Getenv("URLSCAN_API_KEY"),
-	"dnsdumpster": os.Getenv("DNSDUMPSTER_API_KEY"),
-	"vt":          os.Getenv("VT_API_KEY"),
-	"securitytrails": os.Getenv("SECURITYTRAILS_API_KEY"),
-	"shodan":         os.Getenv("SHODAN_API_KEY"),
-}
+	"urlscan":        "019912bb-87ad-711c-8d30-22de19331622",                             // Replace with your real key
+	"dnsdumpster":    "7daf4ffe6b31de5abdb776e6451d953e7ed092bce2037675e25f2864da7bae26", // Replace with your real key
+	"vt":             "50fb683e6b9b43f1f0901b5d6a20bc1a2c117ca1af8c2c6eed2ee1302e493637",
+	"securitytrails": "Mh734Oqxc7e3EQ_s-ejKDBjSJbYCT3Ju",
+	    "shodan":         "heeh4sJl3zYAYovMO3nL0he26qGbhvLp",}
